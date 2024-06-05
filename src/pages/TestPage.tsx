2aad6a73f8941fe0a87edc665b27b9f8a28baad0
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-06-05 03:16:44 +0300

    update logic

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik, Form } from 'formik';
 import { RootState } from '../state/store';
@@ -7,11 +7,37 @@ import Question from '../components/Question';
 import Timer from '../components/Timer';
 import ProgressBar from '../components/ProgressBar';
 import { Question as QuestionType } from '../utils/questionTypes';
-import { Container, Button, Row, Col } from 'react-bootstrap';
+import { Container, Button, Row, Col, Alert } from 'react-bootstrap';
 
 const TestPage: React.FC = () => {
   const questions = useSelector((state: RootState) => state.test.questions as QuestionType[]);
+  const result = useSelector((state: RootState) => state.test.result);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const dispatch = useDispatch();
+
+  const handleNextQuestion = (values: any) => {
+    const questionId = questions[currentQuestionIndex].id;
+    dispatch(setAnswer({ questionId, answer: values[questionId] }));
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      dispatch(submitTest());
+    }
+  };
+
+  if (result) {
+    return (
+      <Container className="mt-5">
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <Alert variant="success" className="text-center">
+              {result}
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 
   return (
     <Container className="mt-5">
@@ -21,21 +47,16 @@ const TestPage: React.FC = () => {
           <ProgressBar className="mb-4" />
           <Formik
             initialValues={questions.reduce((acc, question) => ({ ...acc, [question.id]: question.type === 'multiple-choice' && question.multiple ? [] : '' }), {})}
-            onSubmit={values => {
-              Object.entries(values).forEach(([questionId, answer]) => {
-                dispatch(setAnswer({ questionId, answer }));
-              });
-              dispatch(submitTest());
-            }}
+            onSubmit={handleNextQuestion}
           >
-            {({ values }) => (
-              <Form>
-                {questions.map((question, index) => (
-                  <div key={index} className="mb-4">
-                    <Question question={question} />
-                  </div>
-                ))}
-                <Button variant="primary" type="submit" className="d-block mx-auto">Submit</Button>
+            {({ values, handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <Question question={questions[currentQuestionIndex]} />
+                </div>
+                <Button variant="primary" type="submit" className="d-block mx-auto">
+                  {currentQuestionIndex < questions.length - 1 ? 'Next' : 'Submit'}
+                </Button>
               </Form>
             )}
           </Formik>
--- src/state/testSlice.ts
+++ src/state/testSlice.ts
@@ -5,6 +5,7 @@ interface TestState {
   questions: Question[];
   answers: Record<string, any>;
   progress: number;
+  result: string;
 }
 
 const initialState: TestState = {
@@ -19,7 +20,7 @@ const initialState: TestState = {
     {
       id: 'q2',
       type: 'short-answer',
-      text: 'What is your name?'
+      text: 'What data type is this: []'
     } as ShortAnswerQuestion,
     {
       id: 'q3',
@@ -29,6 +30,7 @@ const initialState: TestState = {
   ],
   answers: {},
   progress: 0,
+  result: ''
 };
 
 const testSlice = createSlice({
@@ -40,8 +42,21 @@ const testSlice = createSlice({
       state.progress = Math.round((Object.keys(state.answers).length / state.questions.length) * 100);
     },
     submitTest: (state) => {
-      // Handle test submission logic
-      console.log('Test submitted', state.answers);
+      // Проверка ответов и вывод результата
+      let correctAnswers = 0;
+      state.questions.forEach(question => {
+        const userAnswer = state.answers[question.id];
+        if (question.type === 'multiple-choice' && (question as MultipleChoiceQuestion).multiple) {
+          if (JSON.stringify(userAnswer) === JSON.stringify(['HTML', 'CSS', 'JavaScript'])) {
+            correctAnswers++;
+          }
+        } else if (question.type === 'short-answer' && userAnswer.toLowerCase() === 'array') {
+          correctAnswers++;
+        } else if (question.type === 'long-answer' && userAnswer !== '') {
+          correctAnswers++;
+        }
+      });
+      state.result = `You got ${correctAnswers} out of ${state.questions.length} correct`;
     },
   },
 });
